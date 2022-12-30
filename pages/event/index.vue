e3f02f8b58546cfc4c310d362c9dcd32ad86d75c
malolebrin <malolebrin@gmail.com>
2022-12-30 12:21:49 +0100

    :tada: ini events and user list page

---
+++ pages/event/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Events
+</div>
+</template>
+
+<script setup lang="ts">
+</script>
+  
---
+++ pages/user/index.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+  Users
+</div>
+</template>
+
+<script setup lang="ts">
+</script>
