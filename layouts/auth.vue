a1f2323dfcf1c8e7c63929615cdb4eb57852b6f2
malolebrin <malolebrin@gmail.com>
2023-01-02 14:08:46 +0100

    ✨ Auth layout with drawer

---
+++ layouts/auth.vue
@@ -0,0 +1,6 @@
+<template>
+<main class="flex min-h-screen bg-white dark:bg-blue-dark">
+  <MenuDrawer />
+  <slot />
+</main>
+</template>
--- pages/event/index.vue
+++ pages/event/index.vue
@@ -7,4 +7,5 @@
 <script setup lang="ts">
 // const nuxtApp = useNuxtApp()
 // console.log(nuxtApp, '<==== nuxtApp')
+definePageMeta({ layout: 'auth' })
 </script>
