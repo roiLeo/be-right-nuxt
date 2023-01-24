21b5a418c6f3c1fbe5c8fd2bdcde68438a36f7cc
malolebrin <malolebrin@gmail.com>
2023-01-24 09:39:01 +0100

    🎨 drawer is fixed

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -2,7 +2,7 @@
 <main class="flex min-h-screen bg-white dark:bg-blue-dark">
   <MenuDrawer />
 
-  <div class="w-full">
+  <div class="w-full lg:ml-64">
     <HeaderDashboard />
     <slot />
   </div>
