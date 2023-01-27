981fb0a8efc5278046eab2eecd4cc3f9723ce00b
malolebrin <malolebrin@gmail.com>
2023-01-27 14:43:04 +0100

    ✨ Simple 404

---
+++ pages/[...slug].vue
@@ -0,0 +1,29 @@
+<template>
+<div class="flex justify-center min-h-screen">
+  <div class="space-y-8 mt-14">
+    <LogoSimpleLogo />
+    <p>Cette page n'existe pas</p>
+    <div class="flex items-center justify-center space-x-4">
+      <BaseButton
+        @click="goBack"
+      >
+        Retour
+      </BaseButton>
+      <BaseButton
+        :href="{ name: 'index' }"
+        color="red"
+      >
+        Accueil
+      </BaseButton>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+const router = useRouter()
+
+function goBack() {
+  router.back()
+}
+</script>
