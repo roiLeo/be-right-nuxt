5ab740f2f88e4e1f12b7e136fb6c6f61670ac306
malolebrin <malolebrin@gmail.com>
2023-01-27 16:56:07 +0100

    🚧 create employee

--- pages/destinataire/create.vue
+++ pages/destinataire/create.vue
@@ -1,10 +1,23 @@
 <template>
 <PageAuthWrapper>
-  create destinaire
+  <div class="flex px-4 mx-auto mb-8 lg:px-8">
+    <EmployeeForm />
+  </div>
 </PageAuthWrapper>
 </template>
 
 <script setup lang="ts">
+import { useAuthStore } from '~~/store'
+
+const authStore = useAuthStore()
+const { fetchAll } = userHook()
+
+onMounted(async () => {
+  if (authStore.isAuthUserAdmin) {
+    await fetchAll('?limit=9999')
+  }
+})
+
 definePageMeta({
   layout: 'auth',
   isAuth: true,
