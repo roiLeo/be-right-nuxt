46ffc41efb795435f54c78e371c00bc087d26542
malolebrin <malolebrin@gmail.com>
2023-02-27 09:22:48 +0100

    ✨Add types for notifications

---
+++ store/notification/types.ts
@@ -0,0 +1,40 @@
+import type { AnswerType } from '../answer'
+import type { BaseEntity, EventType, UserType } from '~~/types'
+
+export enum NotificationTypeEnum {
+  ANSWER_RESPONSE_ACCEPTED = 'ANSWER_RESPONSE_ACCEPTED',
+  ANSWER_RESPONSE_REFUSED = 'ANSWER_RESPONSE_REFUSED',
+  ANSWER_RESPONSE = 'ANSWER_RESPONSE',
+  EVENT_CREATED = 'EVENT_CREATED',
+  EVENT_COMPLETED = 'EVENT_COMPLETED',
+  EVENT_CLOSED = 'EVENT_CLOSED',
+}
+
+export const NotificationTypeEnumArray = Object.values(NotificationTypeEnum)
+
+export interface EventNotificationType extends BaseEntity {
+  name: NotificationTypeEnum
+  notifications?: NotificationType[]
+  notificationIds: number[]
+  event?: EventType
+  eventId: number
+  answer?: AnswerType
+  answerId: number
+}
+
+export interface NotificationType extends BaseEntity {
+  type: NotificationTypeEnum
+  readAt: Date | null
+  subscriber?: NotificationSubscriptionType
+  subscriberId: number | null
+  eventNotification?: EventNotificationType
+  eventNotificationId: number
+}
+
+export interface NotificationSubscriptionType extends BaseEntity {
+  type: NotificationTypeEnum
+  notifications?: NotificationType[]
+  notificationIds: number[]
+  createdByUser?: UserType
+  createdByUserId: number
+}
