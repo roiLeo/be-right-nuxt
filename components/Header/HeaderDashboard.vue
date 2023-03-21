6fc2fb2c7a4623d905d7c3eed5aebec3e87cce0e
Malo <55061197+MaloLebrin@users.noreply.github.com>
2023-03-21 09:41:16 +0100

    ✨ Add title for every routes (#92)
    * ✨ Add specific layout for account view
    
    * ♻️ Rename routes and create menu inside account layout
    
    * ✨ Add reactive menu for account
    
    * ♻️ Refacto billing cards
    
    * ♻️ Rename base base Card in Account
    
    * ✨Create user's role tag component
    
    * ✨ Pages for account
    
    * ✨ Add user table for company
    
    * ✨ Add modal to create new user
    
    * ✨ Add title for every routes used

--- components/Header/HeaderDashboard.vue
+++ components/Header/HeaderDashboard.vue
@@ -75,27 +75,43 @@ const getOutsideMenuRouteLabel = computed(() => {
     switch (formatedRouteName) {
       case 'evenement-show-id':
         return 'Détail de l\'événement'
-
       case 'evenement-edit-id':
         return 'Modifier l\'événement'
+      case 'evenement-archives':
+        return 'Événements supprimés'
+      case 'evenement-create':
+        return 'Créer un événement'
+
+      case 'groupe-creation':
+        return 'Créer un groupe'
 
       case 'user-show-id':
         return 'Détail de l\'utilisateur'
-
       case 'user-edit-id':
         return 'Modifier l\'utilisateur'
 
       case 'destinataire-show-id':
         return 'Détail du destinataire'
+      case 'destinataire-create':
+        return 'Créer un destinataire'
+      case 'destinataire-edit-id':
+        return 'Modifier le destinataire'
 
-      case 'mon-compte-show-id':
-        return 'Mon compte'
-
-      case 'mon-compte-edit-id':
-        return 'Modifier le compte'
+      case 'mon-compte-notifications':
+        return 'Gestion des notifications'
+      case 'mon-compte-parametre':
+        return 'Gestion des paramètres'
+      case 'mon-compte-plan-billing':
+        return 'Abonnement et facturation'
+      case 'mon-compte-utilisateurs':
+        return 'Gestions des comptes utilisateurs'
 
       case 'addresse-create':
         return 'Ajouter une adresse'
+      case 'addresse-edit-id':
+        return 'Modifier l\'adresse'
+      case 'address-show-id':
+        return 'Voir l\'adresse'
     }
   }
 })
